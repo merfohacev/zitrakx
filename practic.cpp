b0dd5b21efe9d9226937c711512b5315dbeee390
merfohacev <74780443+merfohacev@users.noreply.github.com>
2023-09-30 10:42:28 +0700

    рефакторинг

--- practic.cpp
+++ practic.cpp
@@ -1,58 +1,11 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
+#include
 #include <stdio.h>
 
 using namespace std;
 
-struct Date {
-	string year;
-	string month;
-	string day;
-
-	Date() {
-		year = month = day = "";
-	}
-
-};
-
-class planet {
-
-public:
-
-	planet() {
-		radius = 0;
-	}
-
-
-private:
-	string name;
-	Date date;
-	double radius;
-
-
-	friend ostream& operator<< (ostream& os, planet& planet);
-	friend istream& operator>> (istream& is, planet& planet);
-
-};
-
-
-ostream& operator<< (ostream& os, planet& planet)
-{
-	os << "Название: " << planet.name << "\tДата: " << planet.date.year << "." << planet.date.month << "." << planet.date.day << "\tРадиус: " << planet.radius;
-	return os;
-}
-
-istream& operator>> (istream& is, planet& planet)
-{
-	is >> planet.name;
-	planet.name = planet.name.substr(1, planet.name.size() - 2);
-	getline(is, planet.date.year, '.');
-	getline(is, planet.date.month, '.');
-	is >> planet.date.day >> planet.radius;
-
-	return is;
-}
 
 int main() {
 	setlocale(LC_ALL, "Ru");
